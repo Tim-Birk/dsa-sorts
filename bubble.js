441a9f414cf72438e7df77b222c10c3737f82c96
Tim Birkmire <timbirkmire@yahoo.com>
2021-12-26 13:11:57 -0500

    Initial commit and do bubble sort

---
+++ bubble.js
@@ -0,0 +1,14 @@
+function bubbleSort(data) {
+  // Loop with i from end of array towards beginning
+  for (let i = data.length; i > 0; i--) {
+    // Loop with j from the beginning until i - 1
+    for (let j = 0; j < data.length; j++) {
+      // If data[j] is greater than data[j+1], swap those two values!
+      if (data[j] > data[j + 1]) {
+        [data[j], data[j + 1]] = [data[j + 1], data[j]];
+      }
+    }
+  }
+}
+
+module.exports = bubbleSort;
